121fdf9a8daf950c22ed320792b5df526cea3fb5
fengliangjun <fengliangjun@huawei.com>
2025-03-03 01:14:02 +0000

    !5 add config base Merge pull request !5 from fengliangjun/master

--- mindspeed_rl/config_cls/__init__.py
+++ mindspeed_rl/config_cls/__init__.py
@@ -1,2 +1,6 @@
 # coding=utf-8
 # Copyright (c) 2025, HUAWEI CORPORATION. All rights reserved.
+
+from .megatron_config import MegatronConfig
+
+__all__ = ['MegatronConfig']
---
+++ mindspeed_rl/config_cls/megatron_config.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# Copyright (c) 2025, HUAWEI CORPORATION. All rights reserved.
+
+from typing import Dict
+
+from .base_config import BaseConfig
+
+
+class MegatronConfig(BaseConfig):
+    '''
+    Model configuration class.
+
+    models_parameters:
+    use_mcore_models: Whether to use MCore models (default: True)
+    '''
+
+    def __init__(self, config_dict: Dict, model_config_dict: Dict):
+        '''
+        Initialize model configuration from the provided config dictionary.
+        All instance attributes are initialized using the dictionary keys.
+
+        config_dict: Dictionary containing the configuration parameters
+        model_config_dict: a dict of model configuration
+        '''
+        # Default values can still be defined if no config is provided
+        self.use_mcore_models = True
+
+        self.update(config_dict, model_config_dict)

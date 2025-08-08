703df065d073ae6cb6e49ef637dc3a43ad55e3c6
白超 <baichao19@huawei.com>
2025-08-08 03:48:30 +0000

    !525 [feat:verl_npu] add rl-plugin of verl_npu Merge pull request !525 from 白超/master

---
+++ rl-plugin/verl_npu/__init__.py
@@ -0,0 +1,4 @@
+from verl_npu.plugin import apply_npu_plugin
+
+
+apply_npu_plugin()
---
+++ rl-plugin/verl_npu/plugin.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, HUAWEI CORPORATION.  All rights reserved.
+
+from verl_npu.workers.rollout.vllm_rollout.vllm_rollout_spmd import vLLMRolloutPatch
+
+
+def apply_npu_plugin():
+    # Please make sure to apply the patches 
+    # in the correct order so that they can 
+    # work properly.
+    vLLMRolloutPatch.apply_patch()

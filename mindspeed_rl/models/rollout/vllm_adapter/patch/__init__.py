058aafa794ac583fa45b0b9229ad52495b7612a8
wanghao <wanghao773@h-partners.com>
2025-08-14 11:22:22 +0000

    !510 【多模态】【feat.】Support multimodal mrope Merge pull request !510 from wanghao/mrope

--- mindspeed_rl/models/rollout/vllm_adapter/patch/__init__.py
+++ mindspeed_rl/models/rollout/vllm_adapter/patch/__init__.py
@@ -2,7 +2,8 @@
 
 from mindspeed_rl.utils.utils import is_multimodal
 from .qwen2_5_vl_visionmlp_patch import replace_qwen2_5_visionmlp
-
+from .rotary_embedding_patch import replace_qwenvl_mrope
 
 if is_multimodal():
     replace_qwen2_5_visionmlp()
+    replace_qwenvl_mrope()
